6860afe0cc7c1acb6fa8e945161d5190535aa473
suriyaprakash2512 <suriyaprakash.r@mphasis.com>
2023-11-08 16:41:56 +0530

    assign3-ps1

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assign3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //try parse method:
+
+            Console.WriteLine("Enter a number to convert to an Integer:");
+            string numberString = Console.ReadLine();
+            int number;
+
+            if (int.TryParse(numberString, out number))
+            {
+                Console.WriteLine("Conversion successful. Integer value: " + number);
+            }
+            else
+            {
+                Console.WriteLine("Conversion failed. Invalid input for Integer.");
+            }
+
+
+            //convert method
+
+            Console.WriteLine("Enter a number to convert to a Double: ");
+            string doubleString = Console.ReadLine();
+            try
+            {
+                double convertedDouble = Convert.ToDouble(doubleString);
+                Console.WriteLine("Conversion successful. Double value: " + convertedDouble);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Conversion failed. Invalid input for Double.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Conversion failed. Input is too large for Double.");
+            }
+            Console.ReadKey();
+
+
+            //parse method
+
+            Console.WriteLine("\nEnter a number to convert to a Decimal:");
+            string decimalString = Console.ReadLine();
+            try
+            {
+                decimal convertedDecimal = decimal.Parse(decimalString);
+                Console.WriteLine("Conversion successful. Decimal value: " + convertedDecimal);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Conversion failed. Invalid input for Decimal.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Conversion failed. Input is too large for Decimal.");
+            }
+
+        }
+    }
+}
